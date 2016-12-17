1ec434f5dcff74c27ab846dd38a503bf025da146
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2016-12-17 19:00:21 +0100

    Release v2.3.0.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("POGOProtos.Properties")]
+[assembly: AssemblyTitle("POGOProtos")]
 [assembly: AssemblyDescription("A central repository for all proto files of PokémonGO.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AeonLucid")]
