ecbde315646733fa24bc1a69774dde9ad7d73514
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2017-10-14 10:28:15 +0200

    0.77.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.16.0.0")]
+[assembly: AssemblyVersion("2.17.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("v2.16.0.0")]
+[assembly: AssemblyInformationalVersion("v2.17.0.0")]
