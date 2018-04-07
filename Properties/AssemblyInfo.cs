f2911939392c231ad1c423b8a1ae55747f6ccc7a
Furtif <msntchat@hotmail.fr>
2018-04-07 15:40:23 +0200

    2.24.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.23.1.0")]
-[assembly: AssemblyFileVersion("2.23.0.0")]
-[assembly: AssemblyInformationalVersion("v2.23.1.0")]
+[assembly: AssemblyVersion("2.24.0.0")]
+[assembly: AssemblyFileVersion("2.24.0.0")]
+[assembly: AssemblyInformationalVersion("v2.24.0.0")]
